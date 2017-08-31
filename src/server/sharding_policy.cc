b1cb56dc41e60461dfee517eb8806fed4a2ea751
Jiamin Huang <hjmsens@gmail.com>
2017-08-30 20:11:23 -0400

    Add a flag to set prefix separator.

--- src/server/sharding_policy.cc
+++ src/server/sharding_policy.cc
@@ -1,12 +1,17 @@
 #include "sharding_policy.h"
 
+#include "gflags/gflags.h"
+
 #include <string>
+
+DECLARE_string(prefix_seperator);
 
 namespace sqpkv {
 
 int ShardingPolicy::ExtractKey(const rocksdb::Slice &key) {
+  char separator = FLAGS_prefix_seperator[0];
   size_t id_start = 0;
-  while (id_start < key.size_ && key[id_start++] != '_') {
+  while (id_start < key.size_ && key[id_start++] != separator) {
     // Left empty.
   }
   if (id_start == key.size_) {
--- src/server/table_prefix_transform.cc
+++ src/server/table_prefix_transform.cc
@@ -1,20 +1,26 @@
 #include "table_prefix_transform.h"
 
+#include "gflags/gflags.h"
 #include "rocksdb/slice.h"
+
+DEFINE_string(prefix_seperator, ":", "A one-character seperate for the prefix of keys.");
 
 namespace sqpkv {
 
 rocksdb::Slice TablePrefixTransform::Transform(const rocksdb::Slice& key) const {
+  char seperator = FLAGS_prefix_seperator[0];
   for (size_t i = 0; i < key.size_; i++) {
-    if (key[i] == '_') {
+    if (key[i] == seperator) {
       return rocksdb::Slice(key.data_, i);
     }
   }
+  return rocksdb::Slice(key.data_, 0);
 }
 
 bool TablePrefixTransform::InDomain(const rocksdb::Slice& key) const {
+  char seperator = FLAGS_prefix_seperator[0];
   for (size_t i = 0; i < key.size_; i++) {
-    if (key[i] == '_') {
+    if (key[i] == seperator) {
       for (size_t j = i + 1; j < key.size_; j++) {
         if (!std::isdigit(key[j])) {
           return false;
